825beb2da5fadb6cdd1eb8cc7e50805c07f847fa
Muharrem Sait Çoktaş <coktasmuharremsait@gmail.com>
2023-10-18 23:00:40 +0300

    Change layout, Fix flexbox and responsive issues, Add home product card slider, swiper and a little jquery

---
+++ home-product-card-slider-script.js
@@ -0,0 +1,83 @@
+const homeProductCardSlider = () => {
+    const pagination = document.createElement("div");
+    pagination.classList.add("pagination");
+  
+    const data = [
+      {
+        id: 1,
+        imgUrl:
+          "https://ideacdn.net/idea/cq/36/myassets/products/430/eachine-e8-mavi.jpg?revision=1697143329&AppLanguageCode=tr",
+        description: "Badduck DM - 07 1080P HD Kameralı Katlanabilir FPV Drone",
+        title: "YENİ HOBİ DRONU",
+      },
+      {
+        id: 2,
+        imgUrl:
+          "https://ideacdn.net/idea/cq/36/myassets/products/637/x7-access-black-5-600x600.jpg?revision=1697143329&AppLanguageCode=tr",
+        description: "Drone Kumandaları , Rc Drone Kumandaları , Drone Malzemeleri",
+        title: "FrSky QX7 Drone Kumandası-Siyah",
+      },
+      {
+        id: 3,
+        imgUrl:
+          "https://ideacdn.net/idea/cq/36/myassets/products/532/matek-mini-power-hub-lead.jpg?revision=1697143329&AppLanguageCode=tr",
+        description:
+          "Drone ESC Modelleri , Drone Parçaları",
+        title: "Güç dağıtım kartı Matek Mini PDB/ BEC 5V & 12v",
+      },
+      {
+        id: 4,
+        imgUrl:
+          "https://ideacdn.net/idea/cq/36/myassets/products/917/dji-zenmuse-h20-drone-kamerasi-1.jpg?revision=1697143329&AppLanguageCode=tr",
+        description: "Enerji, Güvenlik, İnşaat ve Haritalama Drone Uygulamaları , Drone Kamera ve Gimballeri",
+        title: "DJI Zenmuse H20 Drone Kamerası",
+      },
+    ];
+  
+    // Create the container div
+    const container = document.createElement("div");
+    container.classList.add("swiper-wrapper");
+  
+    // Create and append items to slide div
+    data.forEach((item) => {
+      const slideItem = document.createElement("div");
+      slideItem.classList.add("product-item");
+      slideItem.classList.add("swiper-slide");
+
+      const itemImage = document.createElement("div");
+      itemImage.classList.add("product-item-image");
+
+      const image = document.createElement("img");
+      image.src = item.imgUrl;
+      itemImage.appendChild(image);
+
+      const content = document.createElement("div");
+      content.classList.add("product-item-content");
+  
+      const title = document.createElement("div");
+      title.classList.add('product-item-title');
+      title.textContent = item.title;
+  
+      const description = document.createElement("div");
+      description.classList.add('product-item-description');
+      description.textContent = item.description;
+  
+      const button = document.createElement("a");
+      button.textContent = "Ürüne Git";
+  
+      slideItem.appendChild(itemImage);
+      content.appendChild(title);
+      content.appendChild(description);
+      content.appendChild(button);
+      slideItem.appendChild(content);
+      container.appendChild(slideItem);
+  });
+  
+    // Add the container to the DOM (assuming there is an element with id "root" where you want to mount the slider)
+    document.getElementById("home-product-card-slider-root").appendChild(container);
+    document.getElementById("home-product-card-slider-root").appendChild(pagination);
+  };
+  
+  // Call the slider function to initialize slider
+  homeProductCardSlider();
+  
--- script.js
+++ script.js
@@ -16,21 +16,21 @@ const slider = () => {
     {
       id: 1,
       imgUrl:
-        "https://ideacdn.net/idea/cq/36/themes/selftpl_5fc48b7fbab49/assets/uploads/entry_slider_image_3.png?revision=7.2.7.1-9-1696857147",
+        "https://assets.website-files.com/643f03b9f767055f60e2cdc8/643f0f099e03640fa5b6063e_BG%20Gradient-p-1080.jpg",
       description: "Badduck DM - 07 1080P HD Kameralı Katlanabilir FPV Drone",
       title: "YENİ HOBİ DRONU",
     },
     {
       id: 2,
       imgUrl:
-        "https://ideacdn.net/idea/cq/36/themes/selftpl_5fc48b7fbab49/assets/uploads/entry_slider_image_5.png?revision=7.2.7.1-9-1696857147",
+        "https://images.unsplash.com/photo-1623041997614-064bb5b95af6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8ZHJvbmVzfGVufDB8fDB8fHww&w=1000&q=80",
       description: "DJI - Agriculture",
       title: "TÜRKİYE RESMİ SATICISI",
     },
     {
       id: 3,
       imgUrl:
-        "https://ideacdn.net/idea/cq/36/themes/selftpl_5fc48b7fbab49/assets/uploads/entry_slider_image_8.png?revision=7.2.7.1-9-1696857147",
+        "https://images.pexels.com/photos/6726582/pexels-photo-6726582.jpeg?cs=srgb&dl=pexels-magic-k-6726582.jpg&fm=jpg",
       description:
         "Milli Eğitim Bakanlığı ve SHGM onaylı İHA-0 ve İHA-1 Dron pilotluğu eğitimleri",
       title: "MEB VE SHGM ONAYLI EĞİTİMLER",
@@ -38,21 +38,21 @@ const slider = () => {
     {
       id: 4,
       imgUrl:
-        "https://ideacdn.net/idea/cq/36/themes/selftpl_5fc48b7fbab49/assets/uploads/entry_slider_image_9.png?revision=7.2.7.1-9-1696857147",
+        "https://miro.medium.com/v2/resize:fit:2000/0*nKyTo-_sOA7L0Hdl",
       description: "Türkiye Resmi Distribütörü",
       title: "CHASING",
     },
     {
       id: 5,
       imgUrl:
-        "https://ideacdn.net/idea/cq/36/themes/selftpl_5fc48b7fbab49/assets/uploads/entry_slider_image_10.png?revision=7.2.7.1-9-1696857147",
+        "https://www.powerengineeringint.com/wp-content/uploads/2020/09/DJI-Matrice-300-RTK-DJI-W.jpg",
       description: "Türkiye Resmi Distribütörü",
       title: "EMLID",
     },
     {
       id: 6,
       imgUrl:
-        "https://ideacdn.net/idea/cq/36/themes/selftpl_5fc48b7fbab49/assets/uploads/entry_slider_image_2.png?revision=7.2.7.1-9-1696857147",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9ULhgJ0tYZy_3Bs8Oyh_TLYarg0omBSOlzHKCV4Fp23h0ANQxxI_GUtTRLcCaBR9oa6U&usqp=CAU",
       description:
         "Şimdi ön sipariş verip stoklarla sınırlı indirimden sen de faydalan!",
       title: "DJI MATRICE 30T DE İLK 10 MÜŞTERİYE ÖZEL %30 İNDİRİM",
